4ebbdf949a875bcd068d1cbc8ee9694902cf3c91
amiaxys <amyuqiliang@gmail.com>
2022-04-15 17:04:20 -0400

    debug accordion

--- accordion.js
+++ accordion.js
@@ -1,7 +1,6 @@
 var accordion = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < accordion.length; i++) {
+for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function() {
     this.classList.toggle("active");
 
