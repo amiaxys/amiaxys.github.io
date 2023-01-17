37c3e50c6107317e91627d4ed0b2f43378f16fdc
amiaxys <amyuqiliang@gmail.com>
2023-01-17 18:30:33 -0500

    clean up accordion

--- accordion.js
+++ accordion.js
@@ -1,14 +1,18 @@
-let accordion = document.getElementsByClassName("accordion");
+(function () {
+  window.addEventListener("DOMContentLoaded", function () {
+    let accordion = document.getElementsByClassName("accordion");
 
-for (let i = 0; i < accordion.length; i++) {
-  accordion[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    for (let i = 0; i < accordion.length; i++) {
+      accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
 
-    let body = this.nextElementSibling;
-    if (body.style.maxHeight) {
-      body.style.maxHeight = null;
-    } else {
-      body.style.maxHeight = body.scrollHeight + "px";
+        let body = this.nextElementSibling;
+        if (body.style.maxHeight) {
+          body.style.maxHeight = null;
+        } else {
+          body.style.maxHeight = body.scrollHeight + "px";
+        }
+      });
     }
   });
-}
+})();
